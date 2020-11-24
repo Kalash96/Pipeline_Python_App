2846839af3db82410c6e037d8dbba4c19ba2b6e0
Kalash96 <56406353+Kalash96@users.noreply.github.com>
2020-11-24 13:28:27 +0100

    Update views.py

--- App/views.py
+++ App/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. Here is a test of Azure!!!")
+    return HttpResponse("Hello, world. Here is a test of Azure!!!!!!!!")
 
 def add_numbers(x, y):
     return x + y
