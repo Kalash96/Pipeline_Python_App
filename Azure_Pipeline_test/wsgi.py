764d9d7f8cec80bed220152313c5ff0713d2c804
Mohammed Abdulwahab Kalash <mohammed_abdulwahab.kalash@student.reutlingen-university.de>
2020-11-24 15:35:46 +0100

    Add path to wsgi

--- Azure_Pipeline_test/wsgi.py
+++ Azure_Pipeline_test/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Azure_Pipeline_test.settings')
+
+path_ = '..\antenv\lib\site-packages\django'
+
+if path_ not in sys.path:
+    sys.path.insert(0, path_)
 
 application = get_wsgi_application()
